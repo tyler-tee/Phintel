df2ce7d9ca59e23e03d38019c3126e11ce6e8a0a
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2020-06-18 20:37:10 -0400

    Phishtank_Query Function

--- phintel_funcs.py
+++ phintel_funcs.py
@@ -3,6 +3,7 @@ from config import *
 
 phishtank_api_key = phishtank_api_key
 phishtank_baseapi = "http://checkurl.phishtank.com/checkurl/"
+phishtank_db_url = f"http://data.phishtank.com/data/{phishtank_api_key}/online-valid.json.bz2"
 
 phishtank_headers = {
     "User-Agent": "phishtank/phintel"
@@ -16,10 +17,11 @@ def phishtank_query(phishing_url):
         "app_key": phishtank_api_key
     }
 
-    response = requests.get(phishtank_baseapi, headers=phishtank_headers,
-                            params=params)
+    response = requests.post(phishtank_baseapi, headers=phishtank_headers,
+                             params=params)
 
     if response.status_code == 200:
-        response = response.json()
+        if 'You must supply' not in response.text:
+            return response
 
     return response
