f22309c06131854860bf1abbbaeb0a44d523ece7
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2020-06-23 16:43:24 -0400

    Drastically shortened/simplified custom tokenizer. Current accuracy 67%.

--- phintel_predict.py
+++ phintel_predict.py
@@ -1,28 +1,18 @@
-import math
 import numpy as np
 import pandas as pd
 import random
-from collections import Counter
+import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
 def get_tokens(urls):
-    tokens_by_slash = str(urls.encode('utf-8')).split('/')  # get tokens after splitting by slash
-    all_tokens = []
-    for i in tokens_by_slash:
-        tokens = str(i).split('-')  # get tokens after splitting by dash
-        tokens_by_dot = []
-        for j in range(0, len(tokens)):
-            temp_tokens = str(tokens[j]).split('.')  # get tokens after splitting by dot
-            tokens_by_dot = tokens_by_dot + temp_tokens
-        all_tokens = all_tokens + tokens + tokens_by_dot
-    all_tokens = list(set(all_tokens))  # remove redundant tokens
-    if 'com' in all_tokens:
-        all_tokens.remove(
-            'com')  # removing .com since it occurs a lot of times and it should not be included in our features
-    return all_tokens
+    tokens = list(set(re.split('[/.-]', urls)))
+
+    tokens = [token for token in tokens if token and token != 'com']
+
+    return tokens
 
 
 def TL():
@@ -30,6 +20,8 @@ def TL():
                              error_bad_lines=False)  # reading file
 
     allurlsdata = np.array(df_allurls)  # converting it into an array
+    print(allurlsdata)
+
     random.shuffle(allurlsdata)  # shuffling
 
     categories = [d[1] for d in allurlsdata]  # all labels
@@ -42,14 +34,16 @@ def TL():
 
     lgs = LogisticRegression()  # using logistic regression
     lgs.fit(x_train, y_train)
+
     print(lgs.score(x_test, y_test))
+
     return vectorizer, lgs
 
 
 """
 vectorizer, lgs = TL()
 
-x_predict = ['wikipedia.com']
+x_predict = ['google.com']
 
 x_predict = vectorizer.transform(x_predict)
 
