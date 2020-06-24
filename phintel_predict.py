d1a31cb22c76ac942ee1b6d6df0d698274ecb347
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2020-06-24 16:51:11 -0400

    Improved accuracy of model to 97-98% with ml_dataset.

--- phintel_predict.py
+++ phintel_predict.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pickle
 import random
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 
 def get_tokens(urls):
-    tokens = list(set(re.split('[/.-]', urls)))
+    tokens = list(set(re.split('[/.-?=]', urls)))
 
     tokens = [token for token in tokens if token and token != 'com']
 
@@ -16,38 +17,54 @@ def get_tokens(urls):
 
 
 def TL():
-    df_allurls = pd.read_csv('dataset.csv', dtype={'url': str, 'category': str},
+    df_allurls = pd.read_csv('ml_dataset.csv', dtype={'url': str, 'label': str},
                              error_bad_lines=False)  # reading file
 
     allurlsdata = np.array(df_allurls)  # converting it into an array
-    print(allurlsdata)
 
     random.shuffle(allurlsdata)  # shuffling
 
     categories = [d[1] for d in allurlsdata]  # all labels
     corpus = [str(d[0]) for d in allurlsdata]  # all urls corresponding to a label (either good or bad)
-    vectorizer = TfidfVectorizer(tokenizer=get_tokens)  # get a vector for each url but use our customized tokenizer
+
+    vectorizer = TfidfVectorizer(tokenizer=get_tokens, use_idf=True, smooth_idf=True,
+                                 sublinear_tf=False)  # get a vector for each url but use our customized tokenizer
     x_vector = vectorizer.fit_transform(corpus)  # get the X vector
 
-    x_train, x_test, y_train, y_test = train_test_split(x_vector, categories, test_size=0.2,
-                                                        random_state=42)  # split into training and testing set 80/20
+    model = LogisticRegression(random_state=0)
 
-    lgs = LogisticRegression()  # using logistic regression
-    lgs.fit(x_train, y_train)
+    x_train, x_test, y_train, y_test, indices_train, indices_test = train_test_split(x_vector, categories,
+                                                                                     df_allurls.index, test_size=0.2,
+                                                                                     random_state=0)
+    model.fit(x_train, y_train)
 
-    print(lgs.score(x_test, y_test))
+    clf = LogisticRegression(random_state=0)
+    clf.fit(x_train, y_train)
+    train_score = clf.score(x_train, y_train)
+    test_score = clf.score(x_test, y_test)
+    print(train_score)
+    print(test_score)
 
-    return vectorizer, lgs
+    return vectorizer, clf
 
 
-"""
 vectorizer, lgs = TL()
 
-x_predict = ['google.com']
+
+def save_model():
+    with open('ml_model.pkl', 'wb') as f:
+        pickle.dump(lgs, f)
+    with open('ml_vectorizer.pkl', 'wb') as f:
+        pickle.dump(vectorizer, f)
+
+
+save_model()
+
+
+x_predict = ['sportsauthority.com', 'russiansite.ru/virus.exe', 'wikipedia.com']
 
 x_predict = vectorizer.transform(x_predict)
 
 y_predict = lgs.predict(x_predict)
 
 print(y_predict)
-"""
